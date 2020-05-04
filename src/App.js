1755aa723802eb874121bc444b41edf7db733662
ekotnarowska <ekotnarowska@gmail.com>
2020-05-04 12:42:46 +0200

    layout

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import {HashRouter, Switch, Route} from "react-router-dom";
 
 import Home from "./components/Home";
 import Registration from "./components/Registration";
-import "./scss/main.scss"
 import Login from "./components/Login"
+
+const NotFound = () => {
+    return <h1>:( nie znaleziono</h1>
+}
 
 const App = () => {
     return (
@@ -13,15 +16,12 @@ const App = () => {
 
                 <Switch>
                     <Route exact path="/" component={Home}/>
-                    <Route path="/login" components={Login}/>
-                    <Route path ="/register" components={Registration}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path ="/register" component={Registration}/>
                     <Route component={NotFound}/>
                 </Switch>
             </>
         </HashRouter>
     );
-}
-const NotFound = () => {
-    return <h1>:( nie znaleziono</h1>
 }
 export default App;
