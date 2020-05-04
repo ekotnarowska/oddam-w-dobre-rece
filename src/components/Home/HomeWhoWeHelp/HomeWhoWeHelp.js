8d9f0c3fff516ca135f41bffb2d606566a7ec346
ekotnarowska <ekotnarowska@gmail.com>
2020-05-04 15:28:04 +0200

    layout

--- src/components/Home/HomeWhoWeHelp/HomeWhoWeHelp.js
+++ src/components/Home/HomeWhoWeHelp/HomeWhoWeHelp.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 
 
@@ -6,8 +8,18 @@ const HomeWhoWeHelp = () => {
 
     return (
         <>
-            <section id="foundationAndOrganization">
-            <h1>HomeWhoWeHelp</h1>
+            <section id="foundationAndOrganization" className="foundation">
+                <div className="container">
+                    <h2 className="foundation__title">Komu pomagamy?</h2>
+                    <ul className="foundation__list">
+                        <li className="foundation__item"><Link to="/foundation">Fundacjom</Link></li>
+                        <li className="foundation__item"><Link to="/organization">Organizacjom pozarządowym</Link></li>
+                        <li className="foundation__item"><Link to="local/collection">Lokalnym zbiórkom</Link></li>
+                    </ul>
+                    <p className="foundation__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+                    </p>
+                </div>
+
                 </section>
         </>
     )
