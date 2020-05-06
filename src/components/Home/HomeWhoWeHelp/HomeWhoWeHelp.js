2b5664ab6d754db3a14ff7694b7189040f2e51b7
ekotnarowska <ekotnarowska@gmail.com>
2020-05-06 14:02:14 +0200

    contact form layout

--- src/components/Home/HomeWhoWeHelp/HomeWhoWeHelp.js
+++ src/components/Home/HomeWhoWeHelp/HomeWhoWeHelp.js
@@ -12,9 +12,9 @@ const HomeWhoWeHelp = () => {
                 <div className="container">
                     <h2 className="foundation__title">Komu pomagamy?</h2>
                     <ul className="foundation__list">
-                        <li className="foundation__item"><Link to="/foundation">Fundacjom</Link></li>
-                        <li className="foundation__item"><Link to="/organization">Organizacjom pozarządowym</Link></li>
-                        <li className="foundation__item"><Link to="local/collection">Lokalnym zbiórkom</Link></li>
+                        <li className="foundation__item">Fundacjom</li>
+                        <li className="foundation__item">Organizacjom pozarządowym</li>
+                        <li className="foundation__item">Lokalnym zbiórkom</li>
                     </ul>
                     <p className="foundation__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
                     </p>
