e2d561786ecb94eaf59ccc38519b36b5f44950ce
ekotnarowska <ekotnarowska@gmail.com>
2020-05-15 14:30:17 +0200

    update styles

--- src/components/Login/Logout.js
+++ src/components/Login/Logout.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
-import HomeHeaderMenu from "../Home/HomeHeader/HomeHeaderMenu";
+import HeaderLoginBar from "../Home/HomeHeader/HeaderLoginBar";
 
 const Logout = () => {
     return (
         <div className="logout">
             <div className="container">
-                <HomeHeaderMenu/>
+                <div className="header__top">
+                    <HeaderLoginBar/>
+                </div>
                 <div className="logout__container">
                     <h2 className="logout__title">Wylogowanie nastąpiło pomyślnie!</h2>
                     <Link className="logout__button active" to="/">Strona główna</Link>
--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
-import HomeHeaderMenu from "../Home/HomeHeader/HomeHeaderMenu";
 import HeaderLoginBar from "../Home/HomeHeader/HeaderLoginBar";
 
 
